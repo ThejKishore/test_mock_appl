3872959bf44185116575e7652d1db7654f8537e0
ThejKishore <kishores1984@gmail.com>
2018-09-25 18:21:12 -0500

    added new test cases for jpa

--- src/main/java/com/kish/desig/demoset/ReservationJpa.java
+++ src/main/java/com/kish/desig/demoset/ReservationJpa.java
@@ -2,8 +2,8 @@ package com.kish.desig.demoset;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
+import java.util.List;
 
 public interface ReservationJpa extends JpaRepository<Reservation,Long> {
-    Collection<Reservation> findAllByLastName(String lastName);
+    List<Reservation> findAllByLastName(String lastName);
 }
--- src/test/java/com/kish/desig/demoset/DemosetApplicationTests.java
+++ src/test/java/com/kish/desig/demoset/DemosetApplicationTests.java
@@ -1,18 +1,22 @@
 package com.kish.desig.demoset;
 
+import com.sun.tools.classfile.Type;
 import io.restassured.RestAssured;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Collections;
+import java.util.List;
+
 import static io.restassured.RestAssured.get;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,14 +34,27 @@ public class DemosetApplicationTests {
 
     @RepeatedTest(value = 5 )
     @DisplayName("Initial loading test....")
-    public void contextLoads() {
+    public void testAboutUs() {
         val response = get("/aboutus")
                 .then()
                 .log()
                 .body();
         val responseTxt = response.extract().body().asString();
 //        assertEquals(responseTxt , "About us is up and working");
-        assertThat(responseTxt, equalTo("About us is up and working"));
+        assertThat(responseTxt).isEqualTo("About us is up and working");
+    }
+
+    @Test
+    @DisplayName("get_user_by_lastname")
+    public void getUserByLastname() {
+        val response = get("/reservations/karuneegar")
+                .then()
+                .log()
+                .body();
+        val reservations = response.extract().body().as(List.class);
+        assertThat(reservations).isEqualTo(Collections.EMPTY_LIST);
+
+
     }
 
 }
